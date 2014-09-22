813724bbf23a1aca07691ac3577a7b63c4d4b9a4
AttilaLorincz <attila.lorincz+github@gmail.com>
2014-09-22 23:24:48 +0200

    Program: Whitespace

--- GameAboutSquaresSolver/program.fs
+++ GameAboutSquaresSolver/program.fs
@@ -3,28 +3,32 @@
     open GameAboutSquares.Levels
     open System
     
-    let replaySolution (startState) (solution: Color list) :unit =
-                (startState, solution) ||> List.fold (fun acc step -> (
-                                                                        let newState = makeMoves acc step
-                                                                        printfn "%A" newState
-                                                                        newState
-                                                                      )
-                ) |> ignore
+    let replaySolution (startState) (solution: Color list) : GameState =
+                (startState, solution) 
+                    ||> List.fold (fun acc step -> (
+                                                    let newState = makeMoves acc step
+                                                    printf "%A" newState
+                                                    newState
+                                                   )
+                ) 
     
     [<EntryPoint>]
     let main(args) = 
-        let startState = level 15
-        let maxDepth = 14
+        //Level 15: [Red; Red; Red; Blue; Red; Blue; Red; Red; Blue; Red; Red; Red; ]
+        let startState = level16
+        let maxDepth = 17
         let stopWatch = System.Diagnostics.Stopwatch.StartNew()
         try 
             match solve startState maxDepth with
-                |None -> 
-                    printfn "No solution found in depth %d" maxDepth  
-                    1
-                |Some solution -> 
-                    printf "Solution found:"
-                    printfn "%A" solution
-                    //replaySolution startState solution
-                    0
+            |None -> 
+                printfn "No solution found in depth %d" maxDepth  
+                1
+            |Some solution -> 
+                printf "Solution found: " 
+                printfn "%A" solution
+                //replaySolution startState solution |> ignore
+                printfn "(%A steps)" solution.Length
+                0
         finally
-            printf "Elapsed: %A" stopWatch.Elapsed
+            stopWatch.Stop()
+            printfn "Elapsed: %A" stopWatch.Elapsed
