62586f65ae72088d65cd61f19944a94e7a52504a
AttilaLorincz <attila.lorincz+github@gmail.com>
2014-12-02 21:06:52 +0000

    Command line arguments

--- GameAboutSquaresSolver/program.fs
+++ GameAboutSquaresSolver/program.fs
@@ -1,35 +1,42 @@
 ﻿module GameAboutSqares.Program
-    open GameAboutSquares.Game
-    open GameAboutSquares.Solver
-    open GameAboutSquares.Levels
+
+open GameAboutSquares.Game
+open GameAboutSquares.Solver
+open GameAboutSquares.Levels
+
+let replaySolution (startState) (solution : Color list) : GameState = 
+    (startState, solution) ||> List.fold (fun acc step -> 
+                                   (let newState = makeMoves acc step
+                                    printf "%A" newState
+                                    newState))
+
+[<EntryPoint>]
+let main (args) = 
+    let levelNumber = 
+        if args.Length > 0 then System.Int32.Parse(args.[0])
+        else 
+            System.Console.WriteLine("Select a level to solve (1-35)")
+            System.Int32.Parse(System.Console.ReadLine())
     
-    let replaySolution (startState) (solution: Color list) : GameState =
-                (startState, solution) 
-                    ||> List.fold (fun acc step -> (
-                                                    let newState = makeMoves acc step
-                                                    printf "%A" newState
-                                                    newState
-                                                   )
-                ) 
-    
-    [<EntryPoint>]
-    let main(args) = 
-        //Level 15: [Red; Red; Red; Blue; Red; Blue; Red; Red; Blue; Red; Red; Red; ]
-        //printfn "%A" sizeof<Color>
-        let startState = level 11
-        let maxDepth = 23
-        let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-        try 
-            match solve startState maxDepth with
-            |None -> 
-                printfn "No solution found in depth %d" maxDepth  
-                1
-            |Some solution -> 
-                printfn "Solution found: " 
-                printfn "%A" solution
-                //replaySolution startState solution |> ignore
-                printfn "(%A steps)" solution.Length
-                0
-        finally
-            stopWatch.Stop()
-            printfn "Elapsed: %A" stopWatch.Elapsed
+    let maxDepth = 
+        if args.Length > 1 then System.Int32.Parse(args.[1])
+        else 45
+
+    printfn "Trying to solve level %A in %A steps" levelNumber maxDepth
+
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    try 
+        let startState = level levelNumber
+        match solve startState maxDepth with
+        | None -> 
+            printfn "No solution found in depth %d" maxDepth
+            1
+        | Some solution -> 
+            printfn "Solution found: "
+            printfn "%A" solution
+            //replaySolution startState solution |> ignore
+            printfn "(%A steps)" solution.Length
+            0
+    finally
+        stopWatch.Stop()
+        printfn "Elapsed: %A" stopWatch.Elapsed
