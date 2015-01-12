49ea33bd4e60052b32856157c46fa045c04793c8
AttilaLorincz <attila.lorincz+github@gmail.com>
2015-01-12 17:26:02 +0000

    Refactored main, if no command line arguments provided keep asking for level until 'q' is provided instead of level number

--- GameAboutSquaresSolver/program.fs
+++ GameAboutSquaresSolver/program.fs
@@ -3,28 +3,13 @@
 open GameAboutSquares.Game
 open GameAboutSquares.Solver
 open GameAboutSquares.Levels
+open System 
 
-let replaySolution (startState) (solution : Color list) : GameState = 
-    (startState, solution) ||> List.fold (fun acc step -> 
-                                   (let newState = makeMoves acc step
-                                    printf "%A" newState
-                                    newState))
+let defaultMaxDepth = 45
 
-[<EntryPoint>]
-let main (args) = 
-    let levelNumber = 
-        if args.Length > 0 then System.Int32.Parse(args.[0])
-        else 
-            System.Console.WriteLine("Select a level to solve (0-35)")
-            System.Int32.Parse(System.Console.ReadLine())
-    
-    let maxDepth = 
-        if args.Length > 1 then System.Int32.Parse(args.[1])
-        else 45
-
+let solveLevel levelNumber maxDepth = 
     printfn "Trying to solve level %A in %A steps" levelNumber maxDepth
-
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let stopWatch = Diagnostics.Stopwatch.StartNew()
     try 
         let startState = level levelNumber
         match solve startState maxDepth with
@@ -39,4 +24,22 @@ let main (args) =
             0
     finally
         stopWatch.Stop()
-        printfn "Elapsed: %A" stopWatch.Elapsed
+        printfn "Elapsed: %A\n" stopWatch.Elapsed
+
+let inputFromCommandLine (args : string []) = 
+    let levelNumber = Int32.Parse(args.[0])    
+    let maxDepth = 
+        if args.Length > 1 then Int32.Parse(args.[1])
+        else defaultMaxDepth
+    solveLevel levelNumber maxDepth
+
+let rec repeatedInputFromConsole (lastResult : int) : int = 
+    Console.WriteLine("Select a level to solve (0-35) or 'q' to quit")
+    let readLine = Console.ReadLine()
+    if (String.IsNullOrWhiteSpace(readLine) || readLine = "q" || readLine = "Q") then lastResult
+    else repeatedInputFromConsole (solveLevel (Int32.Parse(readLine)) defaultMaxDepth)
+
+[<EntryPoint>]
+let main (args) = 
+    if args.Length > 0 then inputFromCommandLine args
+    else repeatedInputFromConsole 0
